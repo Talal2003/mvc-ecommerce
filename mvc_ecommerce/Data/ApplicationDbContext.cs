2033b572bca577e147e0113ec42dfffe913c3985
Talal2003 <www.talal2003@gmail.com>
2026-06-05 22:23:55 -0400

    Add ecommerce layout, data models, and seed initial data

---
+++ mvc_ecommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_ecommerce.Models;
+
+namespace mvc_ecommerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=.;Database=MVC14;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                
+                new Category() { Id = 1, Name = "Mobiles"},
+                new Category() { Id = 2, Name = "Tablets" },
+                new Category() { Id = 3, Name = "Laptops" }
+                );
+            modelBuilder.Entity<Product>().HasData(
+
+                new Product() { Id = 1, Name = "Product 1", Description = "This is product 1", Price = 300, Quantity = 100, CategoryId = 1 },
+                new Product() { Id = 2, Name = "Product 2", Description = "This is product 2", Price = 100, Quantity = 55, CategoryId = 1 },
+                new Product() { Id = 3, Name = "Product 3", Description = "This is product 3", Price = 200, Quantity = 20, CategoryId = 1 }
+                );
+        }
+    }
+}
---
+++ mvc_ecommerce/Migrations/20260606020426_productSeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace mvc_ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class productSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "Name", "Price", "Quantity", "Rate" },
+                values: new object[,]
+                {
+                    { 1, 1, "This is product 1", "Product 1", 300m, 100, 0.0 },
+                    { 2, 1, "This is product 2", "Product 2", 100m, 55, 0.0 },
+                    { 3, 1, "This is product 3", "Product 3", 200m, 20, 0.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
